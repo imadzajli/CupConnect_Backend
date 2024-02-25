cc052d62a9a2c9a0107bf935f82e655b06aa98ad
n0t-Me <ahmed.amine.belaroussia@gmail.com>
2024-02-25 23:25:32 +0100

    Added caching with redis

--- devjam/settings.py
+++ devjam/settings.py
@@ -102,6 +102,8 @@ CACHES = {
     }
 }
 
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
