a3f8b421909f36b5a46cd5260a1646e7d46a1dd7
imadzajli <imadzajli6@gmail.com>
2024-02-26 22:35:15 +0100

    3 dishes

--- api/utils/dishes.py
+++ api/utils/dishes.py
@@ -3,36 +3,54 @@ dishes = {
         "a":{
             "name":"Lakhli'i",
             "desc":"Khli'i is considered the favorite dish of people in Fes and its surrounding regions. The name khli'i springs from the term to preserve something for a long time until it becomes ready to be consumed. This type of food is made of kaddid, which is a combination of beef ribs, salt, altsfirh, cumin, coriander, and oil. We cut the beef ribs into thin pieces, mix with the spices and oil, and hang the pieces in a sunny place for up to three days or until they are dried. To make the khli'i, we put the kaddid pieces in fire, then we add a boiled robe with one letter of oil. Finally, it is put in a glass bottle in the refrigerator. It is served hot for lunch or dinner or, as some people prefer, with tea in the morning.",
-            "img":""
+            "img":"https://firebasestorage.googleapis.com/v0/b/devjamxcyberops.appspot.com/o/dishes%2Fkhlii.jpg?alt=media&token=a93858f0-0676-4ca2-819f-8214ae84e190"
         
         },
         "b":{
             "name":"Bastilla",
             "desc":"is served in Fassi houses to welcome guests and to show them kindness and generosity. A favorite dish for important Fassi ceremonies and meetings, it demonstrates a mixture of cooking tradition and art that typifies the culture of Fes. Bastilla is prepared with chicken (or fish), phyllo pastry leaves, eggs, and pecan. After we cook one chicken with onions and spices, we separate the chicken from the spices and add eight or more eggs. Then, we put one pastry leaf in a dish and add the onions and eggs. In the opposite direction we put another leaf and we add the chicken, cut in small pieces. Then we put a third pastry leaf and we add the pecan. After covering the dish completely with a fourth pastry leaf, we bake the bastilla in an oven. After it is cooked and the pastry is browned, we decorate it with sugar and cinnamon.",
-            "img":""
+            "img":"https://firebasestorage.googleapis.com/v0/b/devjamxcyberops.appspot.com/o/dishes%2FBastilla.jpg?alt=media&token=551a3183-ae2a-4d9e-9657-1602a1b3795f"
         },
         "c":{
             "name":"Chebbakia",
             "desc":"Chebbakia, a small piece of twisted dough that is deep fried and soaked in honey, is served during Ramadan as a necessary part of the breakfast meal, but it is also served with harira (a Moroccan soup) during the rest of the year.",
-            "img":""
+            "img":"https://firebasestorage.googleapis.com/v0/b/devjamxcyberops.appspot.com/o/dishes%2Fchebakia.jpg?alt=media&token=1a615dcd-de66-4a86-8d1a-1f189ef59884"
         }
     },
     "Casablance":{
         "a":{
-            "name":"Lakhli'i",
-            "desc":"Khli'i is considered the favorite dish of people in Fes and its surrounding regions. The name khli'i springs from the term to preserve something for a long time until it becomes ready to be consumed. This type of food is made of kaddid, which is a combination of beef ribs, salt, altsfirh, cumin, coriander, and oil. We cut the beef ribs into thin pieces, mix with the spices and oil, and hang the pieces in a sunny place for up to three days or until they are dried. To make the khli'i, we put the kaddid pieces in fire, then we add a boiled robe with one letter of oil. Finally, it is put in a glass bottle in the refrigerator. It is served hot for lunch or dinner or, as some people prefer, with tea in the morning.",
-            "img":""
+            "name":"M'kila",
+            "desc":"While you can find this dish on upscale menus across Casablanca, the best place to eat it is as at the port itself, where the shrimp are fresh and plentiful. Fruity olive oil, tomatoes, cumin, ginger, and herbs create the base. As the mix bubbles over an open burner, small shrimp are tossed in to cook for a few minutes before an egg is quickly poached or scrambled in the brew. Served with a bottomless glass of sweet mint tea and plenty of fresh, crusty Moroccan bread for dipping, this is the perfect breakfast or lunch by the seashore. A number of restaurants serve shrimp with eggs around the port, but the best place to have it isn’t a restaurant at all. Instead, head deep inside the port itself and grab one of the makeshift tables. If you see vendors hustling between tables and mostly Moroccans filling the benches, you’ll know you’re in the right spot.",
+            "img":"https://firebasestorage.googleapis.com/v0/b/devjamxcyberops.appspot.com/o/dishes%2Fmkila.jpg?alt=media&token=e1a80244-d278-487b-81f5-2794f5801ed3"
         
         },
         "b":{
-            "name":"Bastilla",
-            "desc":"is served in Fassi houses to welcome guests and to show them kindness and generosity. A favorite dish for important Fassi ceremonies and meetings, it demonstrates a mixture of cooking tradition and art that typifies the culture of Fes. Bastilla is prepared with chicken (or fish), phyllo pastry leaves, eggs, and pecan. After we cook one chicken with onions and spices, we separate the chicken from the spices and add eight or more eggs. Then, we put one pastry leaf in a dish and add the onions and eggs. In the opposite direction we put another leaf and we add the chicken, cut in small pieces. Then we put a third pastry leaf and we add the pecan. After covering the dish completely with a fourth pastry leaf, we bake the bastilla in an oven. After it is cooked and the pastry is browned, we decorate it with sugar and cinnamon.",
-            "img":""
+            "name":"Snails in Spiced Broth",
+            "desc":"These aren't your French-style escargots dripping in butter and garlic. Not even close. Moroccan snail soup is a favorite Casablanca street food, especially in cooler winter months. Each vendor’s particular snail broth recipe is strictly guarded, with a pinch of this or that to set each seller apart. But some things are universal, like the extensive cleaning process — some claim to rinse the snails at least seven times to rid them of dirt and debris. Then the mollusks are slowly boiled in a liquid that contains as many as 15 herbs and spices, things like cumin seeds, licorice root, mint leaves, thyme, and ginger.",
+            "img":"https://firebasestorage.googleapis.com/v0/b/devjamxcyberops.appspot.com/o/dishes%2Fsnail.png?alt=media&token=880703d0-666b-4127-8319-8b32c4e638c5"
         },
         "c":{
-            "name":"Chebbakia",
-            "desc":"Chebbakia, a small piece of twisted dough that is deep fried and soaked in honey, is served during Ramadan as a necessary part of the breakfast meal, but it is also served with harira (a Moroccan soup) during the rest of the year.",
-            "img":""
+            "name":"Couscous With Seven Vegetables",
+            "desc":"Along with its location on the coast and plentiful seafood, Casablanca is also in the agricultural center of Morocco, and this couscous is a testament to the abundance of crops that grow nearby. Couscous with seven vegetables is eaten throughout Morocco, and is sometimes referred to as couscous bidaoui, a reference to the name of Casablanca in Arabic. The number seven is arbitrary, really — in Arabic, the phrase “seven vegetables” just sounds better than “six,” “eight,” or “10.” The dish itself can include as many or as few vegetables as the cook prefers and still be legit.",
+            "img":"https://firebasestorage.googleapis.com/v0/b/devjamxcyberops.appspot.com/o/dishes%2Fcoscos.png?alt=media&token=d4791b8c-eda1-4afe-b174-b80dbbeb25b1"
+        }
+    },
+     "tangier":{
+        "a":{
+            "name":"Zaazaa",
+            "desc":"To be fair, zaazaas — a sweet parfait-like treat — are available most everywhere in Morocco, but they are a staple in the mahlaba shops of Tangier. These tiny storefronts, usually with just a few tables, sell zaazaa alongside mixed juices and smoothies, as well as occasional small snacks like cookies and cakes.",
+            "img":"https://firebasestorage.googleapis.com/v0/b/devjamxcyberops.appspot.com/o/dishes%2Fzaazaa.jpg?alt=media&token=7e0f24c4-7074-4def-9d4d-0dac133c4622"
+        
+        },
+        "b":{
+            "name":"bocadillos",
+            "desc": "one of the most famous sandwich in morocco",
+            "img":"https://firebasestorage.googleapis.com/v0/b/devjamxcyberops.appspot.com/o/dishes%2Fbocadillo.jpg?alt=media&token=85b92d40-70d3-41ca-9381-a703ac64ebcf"
+        },
+        "c":{
+            "name":"Tagine",
+            "desc":"When you hear the word tagine, your brain instantly conjures up exotic images of colorful dishes cooking over coal or an open fire. You can almost smell the scents steaming from the herbs and spices until it gently dissipates into the cold air. There is definitely something poetic about the process of cooking a tagine, and something intimate about sharing one with friends and family.",
+            "img":"https://firebasestorage.googleapis.com/v0/b/devjamxcyberops.appspot.com/o/dishes%2Ftagine.jpg?alt=media&token=53e42d84-13c9-40b7-bdf9-8bcf8af7fc94"
         }
     },
 
