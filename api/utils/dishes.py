de000cdbb10333fa6483a28da05e557ced74641c
imadzajli <imadzajli6@gmail.com>
2024-02-26 22:11:57 +0100

    comite

---
+++ api/utils/dishes.py
@@ -0,0 +1,40 @@
+dishes = {
+    "Fes":{
+        "a":{
+            "name":"Lakhli'i",
+            "desc":"Khli'i is considered the favorite dish of people in Fes and its surrounding regions. The name khli'i springs from the term to preserve something for a long time until it becomes ready to be consumed. This type of food is made of kaddid, which is a combination of beef ribs, salt, altsfirh, cumin, coriander, and oil. We cut the beef ribs into thin pieces, mix with the spices and oil, and hang the pieces in a sunny place for up to three days or until they are dried. To make the khli'i, we put the kaddid pieces in fire, then we add a boiled robe with one letter of oil. Finally, it is put in a glass bottle in the refrigerator. It is served hot for lunch or dinner or, as some people prefer, with tea in the morning.",
+            "img":""
+        
+        },
+        "b":{
+            "name":"Bastilla",
+            "desc":"is served in Fassi houses to welcome guests and to show them kindness and generosity. A favorite dish for important Fassi ceremonies and meetings, it demonstrates a mixture of cooking tradition and art that typifies the culture of Fes. Bastilla is prepared with chicken (or fish), phyllo pastry leaves, eggs, and pecan. After we cook one chicken with onions and spices, we separate the chicken from the spices and add eight or more eggs. Then, we put one pastry leaf in a dish and add the onions and eggs. In the opposite direction we put another leaf and we add the chicken, cut in small pieces. Then we put a third pastry leaf and we add the pecan. After covering the dish completely with a fourth pastry leaf, we bake the bastilla in an oven. After it is cooked and the pastry is browned, we decorate it with sugar and cinnamon.",
+            "img":""
+        },
+        "c":{
+            "name":"Chebbakia",
+            "desc":"Chebbakia, a small piece of twisted dough that is deep fried and soaked in honey, is served during Ramadan as a necessary part of the breakfast meal, but it is also served with harira (a Moroccan soup) during the rest of the year.",
+            "img":""
+        }
+    },
+    "Casablance":{
+        "a":{
+            "name":"Lakhli'i",
+            "desc":"Khli'i is considered the favorite dish of people in Fes and its surrounding regions. The name khli'i springs from the term to preserve something for a long time until it becomes ready to be consumed. This type of food is made of kaddid, which is a combination of beef ribs, salt, altsfirh, cumin, coriander, and oil. We cut the beef ribs into thin pieces, mix with the spices and oil, and hang the pieces in a sunny place for up to three days or until they are dried. To make the khli'i, we put the kaddid pieces in fire, then we add a boiled robe with one letter of oil. Finally, it is put in a glass bottle in the refrigerator. It is served hot for lunch or dinner or, as some people prefer, with tea in the morning.",
+            "img":""
+        
+        },
+        "b":{
+            "name":"Bastilla",
+            "desc":"is served in Fassi houses to welcome guests and to show them kindness and generosity. A favorite dish for important Fassi ceremonies and meetings, it demonstrates a mixture of cooking tradition and art that typifies the culture of Fes. Bastilla is prepared with chicken (or fish), phyllo pastry leaves, eggs, and pecan. After we cook one chicken with onions and spices, we separate the chicken from the spices and add eight or more eggs. Then, we put one pastry leaf in a dish and add the onions and eggs. In the opposite direction we put another leaf and we add the chicken, cut in small pieces. Then we put a third pastry leaf and we add the pecan. After covering the dish completely with a fourth pastry leaf, we bake the bastilla in an oven. After it is cooked and the pastry is browned, we decorate it with sugar and cinnamon.",
+            "img":""
+        },
+        "c":{
+            "name":"Chebbakia",
+            "desc":"Chebbakia, a small piece of twisted dough that is deep fried and soaked in honey, is served during Ramadan as a necessary part of the breakfast meal, but it is also served with harira (a Moroccan soup) during the rest of the year.",
+            "img":""
+        }
+    },
+
+
+}
