410aac60cf6c31bb34c995ca97e9ba5dbd65eb9c
n0t-Me <ahmed.amine.belaroussia@gmail.com>
2024-02-26 14:25:09 +0100

    Users fixed

--- api/serializer.py
+++ api/serializer.py
@@ -24,7 +24,7 @@ class stadiumsseria(serializers.ModelSerializer):
 class userseria(serializers.ModelSerializer):
     class Meta:
         model = user
-        fields = ("id", "username", "email", "password", "gender", "phone", "country")
+        fields = ("id", "first_name", "last_name", "email", "password", "gender", "phone", "country")
 
 
 class hotelseria(serializers.ModelSerializer):
